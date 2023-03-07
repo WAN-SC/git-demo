2485f5474c0f4b574771f6d39bd5061f5b9fa398
WAN-SC <52846266+WAN-SC@users.noreply.github.com>
2023-03-07 17:53:37 +0800

    Update Test.java

--- src/main/java/com/example/gitdemo/Controller/Test.java
+++ src/main/java/com/example/gitdemo/Controller/Test.java
@@ -9,5 +9,6 @@ public class Test {
         System.out.println("master test");
         System.out.println("branch1 test");
         System.out.println("push test");
+        System.out.println("GitHub test");
     }
 }
