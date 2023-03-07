3d2b64582417d91003860bb7b6af848d82552d4b
wsc <email@email.com>
2023-03-07 10:53:41 +0800

    branch1 commit2

--- src/main/java/com/example/gitdemo/Controller/Test.java
+++ src/main/java/com/example/gitdemo/Controller/Test.java
@@ -6,5 +6,6 @@ public class Test {
         System.out.println("hello git");
         System.out.println("hello git3");
         System.out.println("hello branch1");
+        System.out.println("branch1 test");
     }
 }
