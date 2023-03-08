a41444402a3726fdf1ac250a090a80a454f151c0
wancc63 <wsc603@foxmail.com>
2023-03-08 05:38:54 +0000

    gitee commit2
    Signed-off-by: wancc63 <wsc603@foxmail.com>

--- src/main/java/com/example/gitdemo/Controller/Test.java
+++ src/main/java/com/example/gitdemo/Controller/Test.java
@@ -11,5 +11,6 @@ public class Test {
         System.out.println("push test");
         System.out.println("GitHub test");
         System.out.println("Gitee test");
+        System.out.println("Gitee test2");
     }
 }
